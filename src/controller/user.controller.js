1d8d87eda4e8030d609e75185e1dd5f6c40ab5ea
Arpit Sachan <sachanarpitknp@gmail.com>
2021-12-17 05:18:41 +0530

    user controller file is ready

---
+++ src/controller/user.controller.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const User = require("../models/user.model");
+
+router.get("", async (req, res) => {
+  const user = await User.find().lean().exec();
+  return res.status(200).send({ user });
+});

b2c47f5b205bdf1f49ded17173a529f8a2fd4ea0
Arpit Sachan <sachanarpitknp@gmail.com>
2021-12-17 05:45:04 +0530

    user Schema is ready

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  id: { type: Number },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
   name: { type: String },
+  sex: { type: String },
+  dob: { type: String },
+  country: { type: String },
+  zipcode: { type: Number },
+  height: { type: String },
+  health_data: {
+    weight_goal: { type: String },
+    activity: { type: String },
+    goaled_weight: { type: String },
+  },
 });
 
 const User = mongoose.model("user", userSchema);
